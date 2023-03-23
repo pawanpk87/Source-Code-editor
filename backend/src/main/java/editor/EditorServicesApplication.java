7ca8c210132808ebd76160f66abf26b22a1ba5b8
Pawan Kumar Mehta <arowpk@gmail.com>
2023-03-23 10:28:01 +0530

    Project Setup

---
+++ backend/src/main/java/editor/EditorServicesApplication.java
@@ -0,0 +1,13 @@
+package editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditorServicesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/editor/EditorServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditorServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
